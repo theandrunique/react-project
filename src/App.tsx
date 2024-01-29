8eca2bb96140dc3343ee3bb8260211b99073dadb
theandrunique <theandrunique@gmail.com>
2024-01-29 22:44:59 +0300

    added feature for files transfer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Textarea, TextareaProps } from './components/ui/textarea';
 import Message from './Message';
 import { useState } from 'react';
 import Notification from './Notification';
+import { flushSync } from 'react-dom';
 
 
 const socket = new WebSocket("ws://127.0.0.1:8000/ws/");
@@ -16,8 +17,12 @@ socket.onopen = () => {
 };
 
 function App() {
+    const [drag, setDrag] = useState(false);
+
+
     const [messageText, setMessageText] = useState("");
     const [messages, setMessages] = useState([]);
+    const [files, setFiles] = useState([]);
 
     socket.onmessage = (event) => {
         const new_message = JSON.parse(event.data);
@@ -43,6 +48,33 @@ function App() {
         setMessageText("");
     }
 
+    function dragStartHandler(e){
+        e.preventDefault();
+        setDrag(true);
+    }
+
+    function dragLeaveHandler(e){
+        e.preventDefault();
+        setDrag(false);
+    }
+
+    function onDropHandler(e){
+        e.preventDefault();
+        //let files = [...e.dataTransfer.files];
+
+        setFiles([...e.dataTransfer.files]);
+        //console.log(files);
+
+        setDrag(false);
+    }
+
+    function removeFile(indexToRemove){
+        setFiles((prevFiles) => {
+            const updatedFiles = prevFiles.filter((_, index) => index !== indexToRemove);
+            return updatedFiles;
+          });
+    }
+
     return (
         <>
             <div className='flex justify-center text-center h-full bg-slate-900 '>
@@ -61,12 +93,37 @@ function App() {
                                 />
                             ))}
                     </ScrollArea>
-                    <div className='flex flex-row max-h-36 justify-end space-x-3'>
-                        <Textarea value={ messageText } className='max-h-36' placeholder='Write a message...' onKeyDown={handleEnterKeyDown} onChange={(e) => { setMessageText(e.target.value) }}/>
-                        <Button  size='icon' onClick={ sendMessage }>
-                            <img width="24" height="24" src={iconSend} alt="filled-sent" />
-                        </Button>
-                    </div>
+                    {
+                        drag ? 
+                        <div className='justify-end, border-4 text-fuchsia-300 h-36' 
+                            onDragStart={ (e) => { dragStartHandler(e) } }
+                            onDragLeave={ (e) => { dragLeaveHandler(e) } }
+                            onDragOver={ (e) => { dragStartHandler(e) } }
+                            onDrop={ (e) => { onDropHandler(e) }}
+                        >
+                            Drop here
+                        </div> : 
+
+                        <div className='flex flex-row max-h-36 justify-end space-x-3'
+                            onDragStart={ (e) => { dragStartHandler(e) } }
+                            onDragLeave={ (e) => { dragLeaveHandler(e) } }
+                            onDragOver={ (e) => { dragStartHandler(e) } }
+                        >
+                            <Textarea value={ messageText } className='max-h-36' placeholder='Write a message...' onKeyDown={handleEnterKeyDown} onChange={(e) => { setMessageText(e.target.value) }}/>
+                            <Button  size='icon' onClick={ sendMessage }>
+                                <img width="24" height="24" src={iconSend} alt="filled-sent" />
+                            </Button>
+                        </div>
+                    }
+                    {
+                        files.length !== 0 ? <div className='flex flex-row space-x-3 text-slate-500 m-5'>
+                            {
+                                files.map((file, index) => (
+                                    <div>{file.name}<Button className='text-slate-500 font-bold' onClick={ (e) => { removeFile(index) } }>del</Button></div>
+                                ))
+                            }
+                        </div> : <div></div>
+                    }
                 </div>
             </div>
         </>
