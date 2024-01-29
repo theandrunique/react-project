edc69cf76d27795df1d3f9cc4e2c5d867dcbe2b0
theandrunique <theandrunique@gmail.com>
2024-01-29 18:36:51 +0300

    ~

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,72 @@ import './App.css'
 import { Button } from "@/components/ui/button";
 import { ScrollArea  } from "@/components/ui/scroll-area.tsx";
 import iconSend from "./assets/icon-send.png";
-import { Textarea } from './components/ui/textarea';
+import { Textarea, TextareaProps } from './components/ui/textarea';
 import Message from './Message';
+import { useState } from 'react';
 
 
+const socket = new WebSocket("ws://127.0.0.1:8000/ws/");
+// const user_id: str
+
+socket.onopen = () => {
+    console.log("WebSocket connection opened");
+};
 
 function App() {
-  return (
-    <>
-      <div className='text-center h-full'>
-        <div className='text-center flex flex-col p-5 h-full'>
-          <ScrollArea className='p-5 mb-3 h-full max-w-screen-lg border rounded-md'>
-            <Message/>
-            <Message/>
-            <Message/>
-          </ScrollArea>
-          <div className='flex flex-row max-h-36 justify-end space-x-3 max-w-screen-lg'>
-            <Textarea className='max-h-36' placeholder='Write a message...' />
-            <Button className='' size='icon'>
-              <img width="24" height="24" src={iconSend} alt="filled-sent"/>
-            </Button>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+    const [messageText, setMessageText] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    socket.onmessage = (event) => {
+        const new_message = JSON.parse(event.data);
+        
+        new_message.key = messages.length + 1;
+        setMessages([...messages, new_message]);
+        console.log(messages);
+    };
+
+    function handleEnterKeyDown(e) {
+        if (e.code === "Enter") {
+            e.preventDefault();
+            setMessageText("");
+            sendMessage();
+        }
+    }
+    
+    function sendMessage(){
+        if (messageText.trim().length === 0){
+            return;
+        }
+        socket.send(JSON.stringify({ "message_text": messageText.trim() }));
+     
+        setMessageText("");
+    }
+
+    return (
+        <>
+            <div className='flex justify-center text-center h-full bg-slate-900 '>
+                <div className='text-center flex flex-col p-5 h-full w-full max-w-screen-md'>
+                    <ScrollArea className='p-5 mb-3 flex-grow border rounded-md'>
+                        {/* <Message /> */}
+                        {messages.map((message) => ( <>
+                            <Message 
+                                key={message.key}
+                                message_text={message.message_text}
+                                sender_id={message.from_id}
+                                time={message.sended_at} 
+                                />
+                            </>))}
+                    </ScrollArea>
+                    <div className='flex flex-row max-h-36 justify-end space-x-3'>
+                        <Textarea value={ messageText } className='max-h-36' placeholder='Write a message...' onKeyDown={handleEnterKeyDown} onChange={(e) => { setMessageText(e.target.value) }}/>
+                        <Button  size='icon' onClick={ sendMessage }>
+                            <img width="24" height="24" src={iconSend} alt="filled-sent" />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,26 +1,23 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import photo from "./assets/walrus.jpg"
 
-
-export default function Message(){
-
+export default function Message({ message_text, sender_id, time }){
 
     return (
-        <div className="mb-3 p-3 border rounded-3xl h-auto w-auto flex flex-row space-x-3">
-            <Avatar className="h-14 w-13">
-                <AvatarImage src="https://w.forfun.com/fetch/86/86c9c16ba421f37acc0ca711b96715ee.jpeg" alt="@shadcn" />
+        <div className="bg-slate-800 text-slate-400 mb-3 p-3 border rounded-3xl h-auto w-auto flex flex-row space-x-3">
+            <Avatar className="h-12 w-12">
+                <AvatarImage src={photo} alt="@shadcn" />
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             <div className=" h-auto w-full flex flex-col">
-                <div className="flex flex-row space-x-3">
-                    <div className="font-bold">name</div>
-                    <div>date</div>
+                <div className="text-white flex flex-row space-x-3">
+                    <div className="font-bold">{ sender_id }</div>
+                    <div>{ new Date(Number(time * 1000)).toLocaleString() }</div>
                 </div>
-                <div className="text-wrap h-full w-full text-start">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est cum laboriosam cupiditate, molestiae, amet maxime, vitae officiis earum dolores laborum quisquam corrupti tempore porro nemo sed molestias? Qui, minima eaque.
+                <div className="h-full w-full whitespace-pre-wrap text-start">
+                    { message_text }
                 </div>
             </div>
         </div>
     )
-
-
 }
